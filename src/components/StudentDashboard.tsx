a9e81e99529f35505ca715236e694e6d4cef5461
olakayCoder1 <programmerolakay@gmail.com>
2025-03-19 04:17:37 +0100

    `Removed unused import statement for thumbnail image`

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Video, BookOpen } from 'lucide-react';
-import thumbnail from '../assets/images/thumbnail-im.jpg'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
